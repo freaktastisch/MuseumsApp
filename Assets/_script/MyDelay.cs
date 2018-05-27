a70bd9813353ab6f94c1a16006b1ee774fcf67f4
freaktastisch <39667171+freaktastisch@users.noreply.github.com>
2018-05-27 14:50:40 +0200

    Initial commit

---
+++ Assets/_script/MyDelay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyDelay : MonoBehaviour {
+
+    public float fseconds;
+
+    IEnumerator Example()
+    {
+        Rigidbody rbDelayed = GetComponent<Rigidbody>();
+        yield return new WaitForSeconds(fseconds);
+        rbDelayed.isKinematic = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Example());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
